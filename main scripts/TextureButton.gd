acbef69465b019f14e0548c859b5f4944c8638ca
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-01 15:39:47 -0400

    faded buttons

--- main scripts/TextureButton.gd
+++ main scripts/TextureButton.gd
@@ -1,9 +1,15 @@
 extends TextureButton
 
 func _ready():
+	connect("mouse_entered", Callable(self, "_on_button_hover"))
+	connect("mouse_exited", Callable(self, "_on_button_hover_off"))
 	connect("pressed", Callable(self, "button_pressed"))
 	set_process(false)
 	
+
+func _on_button_hover():
+	get_node("AnimationPlayer").play("hover")
+
 
 func button_pressed():
 	get_tree().change_scene_to_file("res://main scenes/alt_main_menu.tscn")
