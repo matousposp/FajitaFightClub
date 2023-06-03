e90426850426cf8f83e7cfb9244b2e9c47d38dd9
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-03 12:10:41 -0400

    CHARACTER SELECT WORKKSKS

--- main scripts/character_select.gd
+++ main scripts/character_select.gd
@@ -1,12 +1,23 @@
 extends Node2D
+
 signal start
+
+var mike = preload("res://Multiplayer/scenes/player.tscn")
 var selectedChars = 0
+var player1 = false
+var player2 = false
+var player1Char = null
+var player2Char = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if selectedChars == 2:
 		emit_signal("start")
+	
+	if player1 && get_node("selectionBTN").is_pressed():
+		player1Char = mike
+		print("mike")
