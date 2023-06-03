5d2202aed9b19e6510022d5343336dcd736ae5c2
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-03 11:43:38 -0400

    select screen

---
+++ main scripts/selectionBTN.gd
@@ -0,0 +1,18 @@
+extends Button
+signal select
+func _ready():
+	connect("mouse_entered", Callable(self, "_on_button_hover"))
+	connect("mouse_exited", Callable(self, "_on_button_hover_off"))
+	connect("pressed", Callable(self, "button_pressed"))
+	set_process(false)
+	
+
+func _on_button_hover():
+	pass
+
+func _on_button_hover_off():
+	pass
+
+
+func button_pressed():
+	emit_signal("select")
