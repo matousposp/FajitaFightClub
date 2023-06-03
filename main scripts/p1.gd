217ed6de7b390d72521d9a98eee8d095a12f566a
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-02 20:42:40 -0400

    player select start

---
+++ main scripts/p1.gd
@@ -0,0 +1,28 @@
+extends ColorRect
+var characterOne = preload("res://Multiplayer/scenes/player.tscn")
+
+func _ready():
+	connect("mouse_entered", Callable(self, "_on_button_hover"))
+	connect("mouse_exited", Callable(self, "_on_button_hover_off"))
+	set_process_input(true)
+
+func _on_button_hover():
+	pass
+
+func _on_button_hover_off():
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if rect_contains(event.position):
+				pass
+
+func rect_contains(point: Vector2) -> bool:
+	var global_position = get_global_position()
+	var rect_rect = Rect2(global_position, get_size())
+	return rect_rect.has_point(point)
+
+# Instantiate the character displayed on this ColorRect
+# Then, if this is clicked on assign the preloaded characterOne var to global variable which needs to be made
+# Then, this global variable will be used in the actual game scene to instance the specified character node and effectively spawn it in
