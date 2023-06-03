7d8f326806696c0a405a439d706f853784f5d959
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-03 17:25:33 -0400

    map changes

--- main scripts/mapselect.gd
+++ main scripts/mapselect.gd
@@ -19,3 +19,10 @@ func _on_grassland_grass_chosen():
 func _on_space_space_chosen():
 	print("you chose the space map")
 	chosenMap = spacemap
+
+
+func _on_button_pressed():
+	if chosenMap == grassmap:
+		get_tree().change_scene_to_file("res://tmaps/grassmap.tscn")
+	if chosenMap == spacemap:
+		get_tree().change_scene_to_file("res://tmaps/spacemap.tscn")
