95cf17039f9bef901994b3298a11ec9104134512
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-03 12:47:24 -0400

    full char selects

--- main scripts/CheckButton.gd
+++ main scripts/CheckButton.gd
@@ -1,6 +1,11 @@
 extends CheckButton
+var btnOn = false
 
+func _ready():
+	get_parent().player2 = true
 
 
 func _on_toggled(button_pressed):
 	get_parent().player1 = true
+	get_parent().player2 = false
+	btnOn = true
--- main scripts/character_select.gd
+++ main scripts/character_select.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 signal start
-
+var tim = preload("res://assets/1.jpg")
 var mike = preload("res://Multiplayer/scenes/player.tscn")
 var selectedChars = 0
 var player1 = false
@@ -20,6 +20,14 @@ func _process(delta):
 	
 	if player1 && get_node("selectionBTN").is_pressed():
 		player1Char = mike
-		print("mike")
+		print("player 1 chose mike")
 	elif player1 && get_node("selectionBTN2").is_pressed():
-		pass
+		player1Char = tim
+		print("player 1 chose tim")
+	
+	if player2 && get_node("selectionBTN").is_pressed():
+		player2Char = mike
+		print("player 2 chose mike")
+	elif player2 && get_node("selectionBTN2").is_pressed():
+		player2Char = tim
+		print("player 2 chose tim")
