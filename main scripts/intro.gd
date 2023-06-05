53e773c93cfdf3db9d9b6aad0afe85bf1ed1ed22
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-05 16:33:36 -0400

    intro screen

---
+++ main scripts/intro.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_contin_pressed():
+	visible = false
