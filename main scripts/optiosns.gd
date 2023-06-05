44dfc0146cf4381c1a2a90454e4b440b7df8fca0
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-05 14:33:55 -0400

    other

--- main scripts/optiosns.gd
+++ main scripts/optiosns.gd
@@ -3,15 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CanvasLayer.hide()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Paused"):
-		if $CanvasLayer.is_visible():
-			$CanvasLayer.hide()
-			get_tree().paused = false
-		else:
-			$CanvasLayer.show()
-			get_tree().paused = true
+	pass
