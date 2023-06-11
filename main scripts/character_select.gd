c8eef7717088bc2638076b93ccddb5f951414e18
finsld <116563028+finsld@users.noreply.github.com>
2023-06-11 19:58:13 -0400

    imported player tag images

--- main scripts/character_select.gd
+++ main scripts/character_select.gd
@@ -19,15 +19,27 @@ func _process(delta):
 		emit_signal("start")
 	
 	if player1 && get_node("selectionBTN").is_pressed():
+		$p1indicator.visible = true
+		$p1indicator.x = 64
+		$p1indicator.y= 100
 		player1Char = mike
 		print("player 1 chose mike")
 	elif player1 && get_node("selectionBTN2").is_pressed():
+		$p1indicator.visible = true
+		$p1indicator.x = 359
+		$p1indicator.y= 100
 		player1Char = tim
 		print("player 1 chose tim")
 	
 	if player2 && get_node("selectionBTN").is_pressed():
+		$p2indicator.visible = true
+		$p2indicator.x = 64
+		$p2indicator.y = 100
 		player2Char = mike
 		print("player 2 chose mike")
 	elif player2 && get_node("selectionBTN2").is_pressed():
+		$p2indicator.visible = true
+		$p2indicator.x = 359
+		$p2indicator.y= 100
 		player2Char = tim
 		print("player 2 chose tim")
