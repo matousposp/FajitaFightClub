ddab688018823e5ad22c7b6f75dbfb46b5fb2ade
finsld <116563028+finsld@users.noreply.github.com>
2023-06-12 11:56:27 -0400

    character select

--- tmaps/grassmap.gd
+++ tmaps/grassmap.gd
@@ -5,6 +5,8 @@ extends Node2D
 func _ready():
 	print(PlayerData.p1)
 	print(PlayerData.p2)
+	$Mike.position.y = 360
+	$char.position.y = 360
 	if PlayerData.p1 == "mike":
 		$Mike.position.x = 333
 	if PlayerData.p1 == "tim":
@@ -17,4 +19,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$p1indicator.position.x = 60
+	if PlayerData.p1 == "mike":
+		$p1indicator.position.x = $Mike.position.x
+		$p1indicator.position.y = $Mike.position.y - 50
+	if PlayerData.p1 == "tim":
+		$p1indicator.position.x = $char.position.x
+		$p1indicator.position.y = $char.position.y - 50
+	if PlayerData.p2 == "mike":
+		$p2indicator.position.x = $Mike.position.x
+		$p2indicator.position.y = $Mike.position.y - 50
+	if PlayerData.p2 == "tim":
+		$p2indicator.position.x = $char.position.x
+		$p2indicator.position.y = $char.position.y - 50
