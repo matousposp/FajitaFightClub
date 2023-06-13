57da92ac0af39cadd2642b833585eccd07a133fd
finsld <116563028+finsld@users.noreply.github.com>
2023-06-13 18:30:41 -0400

    character select for char

--- main scripts/character_select.gd
+++ main scripts/character_select.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal start
 var tim = preload("res://assets/1.jpg")
 var mike = preload("res://Multiplayer/scenes/player.tscn")
+var char = preload("res://assets/2.jpg")
 var selectedChars = 0
 var player1 = true
 var player2 = false
@@ -34,6 +35,13 @@ func _process(delta):
 		player2 = true
 		PlayerData.p1 = "tim"
 		print("player 1 chose tim")
+	elif player1 && get_node("selectionBTN3").is_pressed():
+		$p1indicator3.visible = true
+		player1 = false
+		player1Char = char
+		player2 = true
+		PlayerData.p1 = "char"
+		print("player 1 chose char")
 	
 	if player2 && get_node("selectionBTN").is_pressed() && player1Char != mike:
 		$p2indicator.visible = true
@@ -45,3 +53,8 @@ func _process(delta):
 		player2Char = tim
 		PlayerData.p2 = "tim"
 		print("player 2 chose tim")
+	elif player2 && get_node("selectionBTN3").is_pressed() && player1Char != char:
+		$p2indicator3.visible = true
+		player2Char = char
+		PlayerData.p2 = "char"
+		print("player 2 chose char")
--- tmaps/grassmap.gd
+++ tmaps/grassmap.gd
@@ -11,13 +11,20 @@ func _ready():
 		$Mike.position.x = 333
 	if PlayerData.p1 == "tim":
 		$char.position.x = 333
+	if PlayerData.p1 == "char":
+		$char.position.x = 333
 	if PlayerData.p2 == "mike":
 		$Mike.position.x = 795
 	if PlayerData.p2 == "tim":
 		$char.position.x = 795
+	if PlayerData.p2 == "char":
+		$char.position.x = 795
 	if not(PlayerData.p1 == "mike" or PlayerData.p2 == "mike"):
 		$Mike.queue_free()
 	if not(PlayerData.p1 == "tim" or PlayerData.p2 == "tim"):
+		#$char.queue_free()
+		pass
+	if not(PlayerData.p1 == "char" or PlayerData.p2 == "char"):
 		$char.queue_free()
 	if not(PlayerData.p1 == "nikkacado" or PlayerData.p2 == "nikkacado"):
 		$nikkacado.queue_free()
