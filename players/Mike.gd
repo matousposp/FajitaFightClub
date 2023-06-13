45dc75a02eeba4f37ce951437a345178bbf598d4
finsld <116563028+finsld@users.noreply.github.com>
2023-06-13 10:55:32 -0400

    stuff

--- players/Mike.gd
+++ players/Mike.gd
@@ -25,7 +25,8 @@ func _physics_process(delta):
 	block -= 1
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		$AnimatedSprite2D.play("jump")
+		if block < 1:
+			$AnimatedSprite2D.play("jump")
 	else:
 		jumps = 2
 	if Input.is_action_just_pressed("normHit"):
@@ -34,17 +35,17 @@ func _physics_process(delta):
 				hit = 20
 			elif hit < 15:
 				$AnimatedSprite2D.play("hit2")
-			elif Input.is_action_just_pressed("special") and not(Input.get_axis("ui_left", "ui_right")):
-				block = 30
-				$AnimatedSprite2D.play("block")
+	if Input.is_action_just_pressed("special"):
+		block = 30
+		$AnimatedSprite2D.play("block")
 
 
 	if Input.is_action_just_pressed("ui_accept") and jumps > 0:
 		$AnimatedSprite2D.play("kick")
 		velocity.y = JUMP_VELOCITY
 		jumps -= 1
-	if is_on_floor() and Input.get_axis("ui_left", "ui_right") == 0:
-		if hit < 1:
+	if is_on_floor() and Input.get_axis("ui_lef t", "ui_right") == 0:
+		if hit < 1 or block < 1:
 			$AnimatedSprite2D.play("idle")
 
 	var direction = Input.get_axis("ui_left", "ui_right")
