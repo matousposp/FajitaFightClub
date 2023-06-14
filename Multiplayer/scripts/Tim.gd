f2d1ebd6e171f5150dab024dcaee7f13f2276550
EvanC22 <evanchin0322@gmail.com>
2023-06-14 11:28:33 -0400

    multiplayer stuff

--- Multiplayer/scripts/Tim.gd
+++ Multiplayer/scripts/Tim.gd
@@ -33,22 +33,22 @@ func _physics_process(delta: float) -> void:
 		motion.y = MAX_FALL_SPEED
 	
 	if Input.is_action_pressed("ui_right"):
-		$AnimatedSprite.flip_h = false
+		$AnimatedSprite2D.flip_h = false
 		if is_on_floor():
-			$AnimatedSprite.play("run")
+			$AnimatedSprite2D.play("run")
 		motion.x = MAX_SPEED
 	elif Input.is_action_pressed("ui_left"):
-		$AnimatedSprite.flip_h = true
+		$AnimatedSprite2D.flip_h = true
 		if is_on_floor():
-			$AnimatedSprite.play("run")
+			$AnimatedSprite2D.play("run")
 		motion.x = -MAX_SPEED
-	elif Input.is_action_pressed("punch"):
-		$AnimatedSprite.play("hit1")
-	elif Input.is_action_pressed("block"):
-		$AnimatedSprite.play("block")
+	elif Input.is_action_pressed("normHit"):
+		$AnimatedSprite2D.play("hit1")
+#	elif Input.is_action_pressed("block"):
+#		$AnimatedSprite.play("block")
 	else:
 		if is_on_floor():
-			$AnimatedSprite.play("default")
+			$AnimatedSprite2D.play("idle")
 		motion.x = 0
 	
 	motion = motion.slide(UP)  
@@ -56,9 +56,9 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		jumps = 2
 	else:
-		$AnimatedSprite.play("jump")
+		$AnimatedSprite2D.play("jump")
 		
-	if Input.is_action_just_pressed("jump") and jumps > 0:
+	if Input.is_action_just_pressed("ui_accept") and jumps > 0:
 		jumps -= 1
 		motion.y = -JUMP_FORCE
 
