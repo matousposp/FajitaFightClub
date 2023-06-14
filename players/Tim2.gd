47c1be43f025e550ac06defa721f49535b0cef4b
EvanC22 <evanchin0322@gmail.com>
2023-06-14 10:56:52 -0400

    Revert "basically got multiplayer to work again, DON"T TOUCH"
    This reverts commit e1156f428f206ab4a7142e97cb1d65cdbd889ca3.

--- players/Tim2.gd
+++ players/Tim2.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-const UP = Vector2(0, -1)
-var GRAVITY = 30
-var MAXFALLSPEED = 1000
-var MAXSPEED = 200
+const UP= Vector2(0, -1) 
+var GRAVITY= 30
+var MAXFALLSPEED= 1000
+var MAXSPEED= 200
 var motion = Vector2()
 var jumps = 2
 var bullet_speed = 10
@@ -16,7 +17,7 @@ var block = 0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _enter_tree() -> void:
+func enter_tree() -> void:
 	set_multiplayer_authority(str(name).to_int())
 
 func _physics_process(delta):
@@ -24,31 +25,27 @@ func _physics_process(delta):
 	$Camera2D.enabled = true
 	hit -= 1
 	block -= 1
-
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		if block < 1:
 			$AnimatedSprite2D.play("jump")
 	else:
 		jumps = 2
+	if Input.is_action_just_pressed("normHit"):
+			if hit < 0:
+				$AnimatedSprite2D.play("hit1")
+				hit = 20
+			elif hit < 15:
+				$AnimatedSprite2D.play("hit2")
+	if Input.is_action_just_pressed("special"):
+		block = 30
+		$AnimatedSprite2D.play("block")
 
-	if Input.is_action_just_pressed("punch"):
-		if hit < 0:
-			$AnimatedSprite2D.play("punch")
-			hit = 20
-		elif hit < 15:
-			pass
-			# $AnimatedSprite2D.play("hit2")
-
-	# if Input.is_action_just_pressed("special"):
-	# 	block = 30
-	# 	$AnimatedSprite2D.play("block")
 
 	if Input.is_action_just_pressed("ui_accept") and jumps > 0:
-		$AnimatedSprite2D.play("jump")
+		$AnimatedSprite2D.play("kick")
 		velocity.y = JUMP_VELOCITY
 		jumps -= 1
-
 	if is_on_floor() and Input.get_axis("ui_left", "ui_right") == 0:
 		if hit < 1 or block < 1:
 			$AnimatedSprite2D.play("idle")
