80f70aa618f99ac3f56b46ce41fcbe08558d1733
finsld <116563028+finsld@users.noreply.github.com>
2023-06-18 15:19:09 -0400

    fixed it again in training

--- training.gd
+++ training.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PlayerData.p1 = "mike"
+	$Mike.p1 = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
