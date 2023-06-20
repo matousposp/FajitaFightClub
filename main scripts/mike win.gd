06a4b34ad76035517212fff495682f1ff8896ceb
matous posp <110625040+matousposp@users.noreply.github.com>
2023-06-19 20:11:26 -0400

    win screens

---
+++ main scripts/mike win.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$T14/AnimationPlayer.play("1")
+	$T16/AnimationPlayer.play("1")
+	$T3/AnimationPlayer.play("1")
+	$T4/AnimationPlayer.play("loop")
+	$T11/AnimationPlayer.play("1")
+	$T9/AnimationPlayer.play("1")
+	$T6/AnimationPlayer.play("1")
+	$T8/AnimationPlayer.play("1")
+	$T2/AnimationPlayer.play("1")
+	$T13/AnimationPlayer.play("1")
+	$"m/AnimationPlayer".play("1")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
